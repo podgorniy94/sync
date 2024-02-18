8c90a1f7dd75c3a52b5708a2dee5ea6cfc0b26e4
podgorniy94 <podgorniy.inc@gmail.com>
2024-02-18 20:00:24 +0100

    1. Added annotation 2. Added try/except blocks 3. Added function docsting 4. Small improvements

--- sync.py
+++ sync.py
@@ -5,6 +5,7 @@ import shutil
 import sys
 import time
 
+# Creating and configuring logging into console and file
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
@@ -20,6 +21,9 @@ logger.addHandler(file_log)
 
 
 def get_hash(file):
+    """
+    Returns the checksum value of a file.
+    """
     BUF_SIZE = 65536
     sha256 = hashlib.sha256()
     with open(file, "rb") as f:
@@ -31,47 +35,57 @@ def get_hash(file):
     return sha256.hexdigest()
 
 
-def get_replica_src_path(root, obj, source, replica):
+def get_rep_src_path(root: str, obj: str, source: str, replica: str) -> tuple[str, str]:
+    """
+    Builds valid paths for copying and automatic cleanup.
+    """
     source_path = os.path.join(root, obj)
     relative_path = os.path.relpath(source_path, source)
     replica_path = os.path.join(replica, relative_path)
     return source_path, replica_path
 
 
-def get_replica_path(root, obj, source, replica):
-    replica_path = get_replica_src_path(root, obj, source, replica)[1]
+def get_replica_path(root: str, obj: str, source: str, replica: str) -> str:
+    replica_path = get_rep_src_path(root, obj, source, replica)[1]
     return replica_path
 
 
-def sync_folders(source_folder, replica_folder, purge=False):
+def sync_folders(source_folder: str, replica_folder: str, purge: bool = False) -> None:
+    """
+    Copies files from the source and automatically performs cleanup in the replica directory for full synchronization.
+    """
     for root, dirs, files in os.walk(source_folder):
         for dir in dirs:
             replica_path = get_replica_path(root, dir, source_folder, replica_folder)
-
-            if not os.path.exists(replica_path):
-                if not purge:
-                    os.makedirs(replica_path)
-                    logging.info(f"Folder created: {replica_path}")
-                else:
-                    dir_path = os.path.join(root, dir)
-                    shutil.rmtree(dir_path)
-                    logging.info(f"Folder deleted: {replica_path}")
+            try:
+                if not os.path.isdir(replica_path):
+                    if not purge:
+                        os.makedirs(replica_path)
+                        logging.info(f"Folder created: {replica_path}")
+                    else:
+                        dir_path = os.path.join(root, dir)
+                        shutil.rmtree(dir_path)
+                        logging.info(f"Folder deleted: {dir_path}")
+            except Exception as e:
+                logging.error(f"Error processing directory '{dir}': {e}")
 
         for file in files:
-            paths = get_replica_src_path(root, file, source_folder, replica_folder)
+            paths = get_rep_src_path(root, file, source_folder, replica_folder)
             source_path, replica_path = paths
-
-            if not os.path.exists(replica_path):
-                if not purge:
+            try:
+                if not os.path.exists(replica_path):
+                    if not purge:
+                        shutil.copy2(source_path, replica_path)
+                        logger.info(f"File created: {replica_path}")
+                    else:
+                        file_path = os.path.join(root, file)
+                        os.remove(file_path)
+                        logger.info(f"File removed: {replica_path}")
+                elif get_hash(source_path) != get_hash(replica_path):
                     shutil.copy2(source_path, replica_path)
-                    logger.info(f"File created: {replica_path}")
-                else:
-                    file_path = os.path.join(root, file)
-                    os.remove(file_path)
-                    logger.info(f"File removed: {replica_path}")
-            elif get_hash(source_path) != get_hash(replica_path):
-                shutil.copy2(source_path, replica_path)
-                logger.info(f"File updated: {replica_path}")
+                    logger.info(f"File updated: {replica_path}")
+            except Exception as e:
+                logging.error(f"Error processing file '{file}': {e}")
 
 
 if __name__ == "__main__":
